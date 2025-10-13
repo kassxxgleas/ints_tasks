f14992a9902d15bc982627c635709b960c509b92
ints_tasks <linktdev@gmail.com>
2025-10-13 20:40:16 +0200

    Commits $ new files

--- AI/cvik3/EDA.py
+++ AI/cvik3/EDA.py
@@ -2,14 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 df = pd.read_csv("AI/cvik3/Titanic-Dataset.csv")
+
+print(df.head())
+
 print(df.shape)
-print(df.info())
+
 print(df.describe())
 
-df.isnull().sum()
+print(df.info())
+# data = pd.read_csv("")
+# data.head()
+# data.info()
+# data.isnull().sum()
+# data['Цена'].fillna(data['Цена'].mean(), inplace=True)
+# data.describe() // mode, mean, median, fillna
+#
+#
+#
+print(df.isnull().sum())
+
 df.groupby('Sex')['Survived'].mean()
 df.groupby('Pclass')['Survived'].mean()
-
+df['Age'].fillna(1, inplace=True)
+print(df.isnull().sum())
 
 plt.hist(df['Age'].dropna(), bins=30)
 plt.title("Age Distribution")
@@ -19,8 +34,3 @@ plt.show()
 
 df[['Fare', 'Survived']].groupby('Survived').mean()
 df.groupby('Embarked')['Survived'].mean()
-
-
-sns.heatmap(df.corr(numeric_only=True), annot=True, cmap="coolwarm")
-plt.title("Correlation Heatmap")
-plt.show()
